5d46124620dcb9defe36b50717876df4b9ef2cc4
Sayali Bonde <Sayali.Bonde@paysafe.com>
2019-07-05 17:59:09 +0530

    Auth Updated

--- lib/paysafe/card_payments/authentication.rb
+++ lib/paysafe/card_payments/authentication.rb
@@ -7,6 +7,8 @@ module Paysafe
       attr_accessor :threeDEnrollment
       attr_accessor :threeDResult
       attr_accessor :signatureStatus
+      attr_accessor :threeDSecureVersion
+      attr_accessor :directoryServerTransactionId
     end
   end
 end
